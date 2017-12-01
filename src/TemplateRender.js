3dd2f56a025653471824588fb060a52cbeac6fdf
Zach Leatherman <zachleatherman@gmail.com>
2017-12-01 08:32:11 -0600

    Cleanup TemplateRender, adds a bunch more tests.

--- src/TemplateRender.js
+++ src/TemplateRender.js
@@ -9,47 +9,65 @@ const pug = require('pug');
 const nunjucks = require('nunjucks');
 const liquidEngine = require('liquidjs')();
 
+// TODO make path and str for template content independent, why do we even need a path here?
 function TemplateRender( path ) {
 	this.parsed = path ? parsePath( path ) : undefined;
+	this.engine = this.parsed && this.parsed.ext ? this.parsed.ext.substr(1) : path;
+	this.defaultMarkdownEngine = "ejs";
 }
+
+TemplateRender.prototype.setDefaultMarkdownEngine = function(markdownEngine) {
+	this.defaultMarkdownEngine = markdownEngine;
+};
 
 TemplateRender.prototype.render = async function(str, data) {
 	let fn = await this.getCompiledTemplatePromise(str);
 	return fn(data);
 };
 
-TemplateRender.prototype.getCompiledTemplatePromise = function(str) {
-	return new Promise(function (resolve, reject) {
-		if( !this.parsed || this.parsed.ext === ".ejs" ) {
-			resolve( ejs.compile(str) );
-		} else if( this.parsed.ext === ".md" ) {
-			(new TemplateRender()).getCompiledTemplatePromise(str).then(function(fn) {
-				resolve(function(data) {
-					return md.render(fn(data));
-				});
-			});
-		} else if( this.parsed.ext === ".hbs" ) {
-			resolve(Handlebars.compile(str));
-		} else if( this.parsed.ext === ".mustache" ) {
-			resolve(function(data) {
-				return Mustache.render(str, data).trim();
-			});
-		} else if( this.parsed.ext === ".haml" ) {
-			resolve(haml.compile(str));
-		} else if( this.parsed.ext === ".pug" ) {
-			resolve(pug.compile(str));
-		} else if( this.parsed.ext === ".njk" ) {
-			let tmpl = new nunjucks.Template(str);
-			resolve(function(data) {
-				return tmpl.render(data);
-			});
-		} else if( this.parsed.ext === ".liquid" ) {
-			let tmpl = liquidEngine.parse(str);
-			resolve(function(data) {
-				return liquidEngine.render(tmpl, data); // is a promise too fuuuuck
-			});
+TemplateRender.prototype.getCompiledTemplatePromise = async function(str, options) {
+	options = Object.assign({
+		parseMarkdownWith: this.defaultMarkdownEngine
+	}, options);
+
+	if( !this.engine || this.engine === "ejs" ) {
+		return ejs.compile(str);
+	} else if( this.engine === "md" ) {
+		if( options.parseMarkdownWith ) {
+			let fn = await ((new TemplateRender(options.parseMarkdownWith)).getCompiledTemplatePromise(str));
+
+			return async function(data) {
+				return md.render(await fn(data));
+			};
+		} else {
+			return function(data) {
+				// do nothing with data if parseMarkdownWith is falsy
+				return md.render(str);
+			};
 		}
-	}.bind(this));
+	} else if( this.engine === "hbs" ) {
+		return Handlebars.compile(str);
+	} else if( this.engine === "mustache" ) {
+		return function(data) {
+			return Mustache.render(str, data).trim();
+		};
+	} else if( this.engine === "haml" ) {
+		return haml.compile(str);
+	} else if( this.engine === "pug" ) {
+		return pug.compile(str);
+	} else if( this.engine === "njk" ) {
+		let tmpl = new nunjucks.Template(str);
+		return function(data) {
+			return tmpl.render(data);
+		};
+	} else if( this.engine === "liquid" ) {
+		let tmpl = await liquidEngine.parse(str);
+		return async function(data) {
+			return await liquidEngine.render(tmpl, data);
+		};
+	} else {
+		throw new Error(engine + " is not a supported template engine.");
+	}
 };
 
 module.exports = TemplateRender;
--- test/TemplateRenderTest.js
+++ test/TemplateRenderTest.js
@@ -1,75 +1,132 @@
 import test from "ava";
 import ejs from "ejs";
 import TemplateRender from "../src/TemplateRender";
+import parsePath from "parse-filepath";
+
+test(t => {
+	// Path is unnecessary but supported
+	t.truthy((new TemplateRender("default.ejs")).parsed);
+	t.is((new TemplateRender("default.ejs")).engine, "ejs");
+
+	// Better
+	t.truthy((new TemplateRender("ejs")).parsed);
+	t.is((new TemplateRender("ejs")).engine, "ejs");
+});
+
+test("Unsupported engine", async t => {
+	t.is( (new TemplateRender( "doesnotexist" )).engine, "doesnotexist" );
+
+	await t.throws( (new TemplateRender( "doesnotexist" )).getCompiledTemplatePromise("<p></p>") );
+});
 
 // EJS
-test(t => {
-	t.is( (new TemplateRender( "default.ejs" )).parsed.ext, ".ejs" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "default.ejs" )).getCompiledTemplatePromise("<p><%= name %></p>");
+test("EJS", async t => {
+	t.is( (new TemplateRender( "ejs" )).engine, "ejs" );
+
+	let fn = await (new TemplateRender( "ejs" )).getCompiledTemplatePromise("<p><%= name %></p>");
 	t.is( await fn({name: "Zach"}), "<p>Zach</p>" );
 });
 
 // Markdown
-test(t => {
-	t.is( (new TemplateRender( "markdown.md" )).parsed.ext, ".md" );
+test("Markdown", t => {
+	t.is( (new TemplateRender( "md" )).engine, "md" );
 });
-test(async t => {
-	let fn = await (new TemplateRender( "markdown.md" )).getCompiledTemplatePromise("# Header");
+
+test("Markdown: Parses base markdown", async t => {
+	let fn = await (new TemplateRender( "md" )).getCompiledTemplatePromise("# Header");
 	t.is( (await fn()).trim(), "<h1>Header</h1>" );
 });
 
-// Handlebars
-test(t => {
-	t.is( (new TemplateRender( "handlebars.hbs" )).parsed.ext, ".hbs" );
+test("Markdown: Parses templates using default engine", async t => {
+	let fn = await (new TemplateRender( "md" )).getCompiledTemplatePromise("# <%=title %>");
+	t.is( (await fn({title: "My Title"})).trim(), "<h1>My Title</h1>" );
 });
-test(async t => {
-	let fn = await (new TemplateRender( "handlebars.hbs" )).getCompiledTemplatePromise("<p>{{name}}</p>");
+
+test("Markdown: Override markdown engine no data", async t => {
+	let fn = await (new TemplateRender( "md" )).getCompiledTemplatePromise("# My Title", {
+		parseMarkdownWith: "liquid"
+	});
+
+	t.is( (await fn()).trim(), "<h1>My Title</h1>" );
+});
+
+test("Markdown: Override markdown engine", async t => {
+	let fn = await ((new TemplateRender( "md" )).getCompiledTemplatePromise("# {{title}}", {
+		parseMarkdownWith: "liquid"
+	}));
+	t.is( (await fn({title: "My Title"})).trim(), "<h1>My Title</h1>" );
+});
+
+test("Markdown: Set markdown engine to false", async t => {
+	let fn = await ((new TemplateRender( "md" )).getCompiledTemplatePromise("# {{title}}", {
+		parseMarkdownWith: false
+	}));
+	t.is( (await fn()).trim(), "<h1>{{title}}</h1>" );
+});
+
+test("Markdown: Change the default engine", async t => {
+	let tr = new TemplateRender( "md" );
+	tr.setDefaultMarkdownEngine("liquid");
+
+	let fn = await (tr.getCompiledTemplatePromise("# {{title}}"));
+	t.is( (await fn({title: "My Title"})).trim(), "<h1>My Title</h1>" );
+});
+
+test("Markdown: Change the default engine and pass in an override", async t => {
+	let tr = new TemplateRender( "md" );
+	tr.setDefaultMarkdownEngine("njk");
+
+	let fn = await (tr.getCompiledTemplatePromise("# {{title}}", {
+		parseMarkdownWith: "liquid"
+	}));
+
+	t.is( (await fn({title: "My Title"})).trim(), "<h1>My Title</h1>" );
+});
+
+// Handlebars
+test("Handlebars", async t => {
+	t.is( (new TemplateRender( "hbs" )).engine, "hbs" );
+
+	let fn = await (new TemplateRender( "hbs" )).getCompiledTemplatePromise("<p>{{name}}</p>");
 	t.is( await fn({name: "Zach"}), "<p>Zach</p>" );
 });
 
 // Mustache
-test(t => {
-	t.is( (new TemplateRender( "mustache.mustache" )).parsed.ext, ".mustache" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "mustache.mustache" )).getCompiledTemplatePromise("<p>{{name}}</p>");
+test("Mustache", async t => {
+	t.is( (new TemplateRender( "mustache" )).engine, "mustache" );
+
+	let fn = await (new TemplateRender( "mustache" )).getCompiledTemplatePromise("<p>{{name}}</p>");
 	t.is( await fn({name: "Zach"}), "<p>Zach</p>" );
 });
 
 // Haml
-test(t => {
-	t.is( (new TemplateRender( "haml.haml" )).parsed.ext, ".haml" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "haml.haml" )).getCompiledTemplatePromise("%p= name");
+test("Haml", async t => {
+	t.is( (new TemplateRender( "haml" )).engine, "haml" );
+
+	let fn = await (new TemplateRender( "haml" )).getCompiledTemplatePromise("%p= name");
 	t.is( (await fn({name: "Zach"})).trim(), "<p>Zach</p>" );
 });
 
 // Pug
-test(t => {
-	t.is( (new TemplateRender( "pug.pug" )).parsed.ext, ".pug" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "pug.pug" )).getCompiledTemplatePromise("p= name");
+test("Pug", async t => {
+	t.is( (new TemplateRender( "pug" )).engine, "pug" );
+
+	let fn = await (new TemplateRender( "pug" )).getCompiledTemplatePromise("p= name");
 	t.is( await fn({name: "Zach"}), "<p>Zach</p>" );
 });
 
 // Nunjucks
-test(t => {
-	t.is( (new TemplateRender( "nunjucks.njk" )).parsed.ext, ".njk" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "nunjucks.njk" )).getCompiledTemplatePromise("<p>{{ name }}</p>");
+test("Nunjucks", async t => {
+	t.is( (new TemplateRender( "njk" )).engine, "njk" );
+
+	let fn = await (new TemplateRender( "njk" )).getCompiledTemplatePromise("<p>{{ name }}</p>");
 	t.is( await fn({name: "Zach"}), "<p>Zach</p>" );
 });
 
 // Liquid
-test(t => {
-	t.is( (new TemplateRender( "liquid.liquid" )).parsed.ext, ".liquid" );
-});
-test(async t => {
-	let fn = await (new TemplateRender( "liquid.liquid" )).getCompiledTemplatePromise("<p>{{name | capitalize}}</p>");
+test("Liquid", async t => {
+	t.is( (new TemplateRender( "liquid" )).engine, "liquid" );
+
+	let fn = await (new TemplateRender( "liquid" )).getCompiledTemplatePromise("<p>{{name | capitalize}}</p>");
 	t.is(await fn({name: "tim"}), "<p>Tim</p>" );
 });
