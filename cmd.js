79891860561dd5bb5dad2e4f512fde2b6eca349b
Zach Leatherman <zachleatherman@gmail.com>
2017-12-14 22:38:59 -0600

    Asyncing all of it

--- cmd.js
+++ cmd.js
@@ -19,63 +19,63 @@ if (argv.formats && argv.formats !== "*") {
   formats = argv.formats.split(",");
 }
 
-let start = new Date();
-
-let data = new TemplateData(argv.data || cfg.globalDataFile);
 (async function() {
+  let start = new Date();
+
+  let data = new TemplateData(argv.data || cfg.globalDataFile);
   await data.cacheData();
-})();
 
-let writer = new TemplateWriter(
-  inputDir,
-  argv.output || cfg.dir.output,
-  formats,
-  data
-);
-
-if (argv.version) {
-  console.log(pkg.version);
-} else if (argv.help) {
-  let out = [];
-  out.push("usage: eleventy");
-  out.push("       eleventy --watch");
-  out.push("       eleventy --input=./templates --output=./dist");
-  out.push("");
-  out.push("arguments: ");
-  out.push("  --version");
-  out.push("  --watch");
-  out.push("       Wait for files to change and automatically rewrite.");
-  out.push("  --input");
-  out.push("       Input template files (default: `templates`)");
-  out.push("  --output");
-  out.push("       Write HTML output to this folder (default: `dist`)");
-  out.push("  --formats=liquid,md");
-  out.push("       Whitelist only certain template types (default: `*`)");
-  out.push("  --data");
-  out.push("       Set your own global data file (default: `data.json`)");
-  // out.push( "  --config" );
-  // out.push( "       Set your own local configuration file (default: `eleventy.config.js`)" );
-  out.push("  --help");
-  out.push("       Show this message.");
-  console.log(out.join("\n"));
-} else if (argv.watch) {
-  console.log("Watching…");
-  var watcher = watch(writer.getFiles().concat(cfg.globalDataFile));
-
-  watcher.on("change", function(path, stat) {
-    console.log("File changed:", path);
-    writer.write();
-  });
-
-  watcher.on("add", function(path, stat) {
-    console.log("File added:", path);
-    writer.write();
-  });
-} else {
-  writer.write();
-  console.log(
-    "Finished in",
-    ((new Date() - start) / 1000).toFixed(2),
-    "seconds"
+  let writer = new TemplateWriter(
+    inputDir,
+    argv.output || cfg.dir.output,
+    formats,
+    data
   );
-}
+
+  if (argv.version) {
+    console.log(pkg.version);
+  } else if (argv.help) {
+    let out = [];
+    out.push("usage: eleventy");
+    out.push("       eleventy --watch");
+    out.push("       eleventy --input=./templates --output=./dist");
+    out.push("");
+    out.push("arguments: ");
+    out.push("  --version");
+    out.push("  --watch");
+    out.push("       Wait for files to change and automatically rewrite.");
+    out.push("  --input");
+    out.push("       Input template files (default: `templates`)");
+    out.push("  --output");
+    out.push("       Write HTML output to this folder (default: `dist`)");
+    out.push("  --formats=liquid,md");
+    out.push("       Whitelist only certain template types (default: `*`)");
+    out.push("  --data");
+    out.push("       Set your own global data file (default: `data.json`)");
+    // out.push( "  --config" );
+    // out.push( "       Set your own local configuration file (default: `.eleventy.js`)" );
+    out.push("  --help");
+    out.push("       Show this message.");
+    console.log(out.join("\n"));
+  } else if (argv.watch) {
+    console.log("Watching…");
+    var watcher = watch(writer.getFiles().concat(cfg.globalDataFile));
+
+    watcher.on("change", function(path, stat) {
+      console.log("File changed:", path);
+      writer.write();
+    });
+
+    watcher.on("add", function(path, stat) {
+      console.log("File added:", path);
+      writer.write();
+    });
+  } else {
+    await writer.write();
+    console.log(
+      "Finished in",
+      ((new Date() - start) / 1000).toFixed(2),
+      "seconds"
+    );
+  }
+})();
