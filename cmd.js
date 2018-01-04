5aa9c2accf544ba2082867c1e9ebb6c01d3e5abe
Zach Leatherman <zachleatherman@gmail.com>
2018-01-03 19:57:54 -0600

    Fixes #16

--- cmd.js
+++ cmd.js
@@ -1,22 +1,41 @@
 #!/usr/bin/env node
 const argv = require("minimist")(process.argv.slice(2));
-const Eleventy = require("./src/Eleventy");
+const versionCheck = require("check-node-version");
 
-let eleven = new Eleventy(argv.input, argv.output);
-eleven.setFormats(argv.formats);
-eleven.setIsVerbose(!argv.quiet);
+function EleventyNodeVersionCheck() {
+  return new Promise(function(resolve, reject) {
+    versionCheck({ node: "8.x.x" }, function(err, result) {
+      if (!result.versions.node.isSatisfied) {
+        console.log(
+          "Eleventy requires Node version 8 or above. You’re currently using " +
+            result.versions.node.version +
+            "."
+        );
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
-(async () => {
-  await eleven.init();
+EleventyNodeVersionCheck().then(function() {
+  const Eleventy = require("./src/Eleventy");
 
-  if (argv.version) {
-    console.log(eleven.getVersion());
-  } else if (argv.help) {
-    console.log(eleven.getHelp());
-  } else if (argv.watch) {
-    await eleven.watch();
-  } else {
-    await eleven.write();
-    console.log(eleven.getFinishedLog());
-  }
-})();
+  let eleven = new Eleventy(argv.input, argv.output);
+  eleven.setFormats(argv.formats);
+  eleven.setIsVerbose(!argv.quiet);
+
+  eleven.init().then(function() {
+    if (argv.version) {
+      console.log(eleven.getVersion());
+    } else if (argv.help) {
+      console.log(eleven.getHelp());
+    } else if (argv.watch) {
+      eleven.watch();
+    } else {
+      eleven.write().then(function() {
+        console.log(eleven.getFinishedLog());
+      });
+    }
+  });
+});
