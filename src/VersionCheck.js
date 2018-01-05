f29ac3b6b7647f44895a9a810707e0281d00ed59
Zach Leatherman <zachleatherman@gmail.com>
2018-01-04 21:50:36 -0600

    Use package.json engines.node for compatibility check.

--- src/VersionCheck.js
+++ src/VersionCheck.js
@@ -1,8 +1,13 @@
+const pkg = require("../package.json");
 const versionCheck = require("check-node-version");
 
+/*
+ * This file should have maximum compatibility to ensure node checks can
+ * report requirements on unsupported versions.
+ */
 module.exports = function() {
   return new Promise(function(resolve, reject) {
-    versionCheck({ node: ">= 8.x.x" }, function(err, result) {
+    versionCheck({ node: pkg.engines.node }, function(err, result) {
       if (!result.versions.node.isSatisfied) {
         console.log(
           "Eleventy requires Node version 8 or above. You’re currently using " +
