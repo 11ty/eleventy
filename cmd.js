4d800bf07333122d22f79b977e23d87bfb0f4c56
Zach Leatherman <zachleatherman@gmail.com>
2018-01-16 07:29:47 -0600

    Adds CLI command to debug output.

--- cmd.js
+++ cmd.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 const argv = require("minimist")(process.argv.slice(2));
 const EleventyNodeVersionCheck = require("./src/VersionCheck");
+const debug = require("debug")("Eleventy-CLI");
 
 EleventyNodeVersionCheck().then(function() {
   const Eleventy = require("./src/Eleventy");
+  debug(
+    "command: eleventy" +
+      (argv.input ? " --input=" + argv.input : "") +
+      (argv.output ? " --output=" + argv.output : "") +
+      (argv.formats ? " --formats=" + arg.formats : "") +
+      (argv.config ? " --config=" + arg.config : "") +
+      (argv.quiet ? " --quiet" : "") +
+      (argv.version ? " --version" : "") +
+      (argv.watch ? " --watch" : "")
+  );
 
   let elev = new Eleventy(argv.input, argv.output);
   elev.setConfigPath(argv.config);
