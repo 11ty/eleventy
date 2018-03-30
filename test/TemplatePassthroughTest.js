a1f600a15121a7058f63b179b6a6802219583286
Mat Marquis <mat@matmarquis.com>
2018-03-30 10:50:56 -0400

    Adds tests for #90.

--- test/TemplatePassthroughTest.js
+++ test/TemplatePassthroughTest.js
@@ -12,3 +12,27 @@ test("Constructor Dry Run", t => {
   pass.setDryRun(true);
   t.is(pass.isDryRun, true);
 });
+
+test("Origin path isn’t included in output when targeting a directory", t => {
+  let pass = new TemplatePassthrough("_src/img", "_site");
+  t.truthy(pass);
+  t.is(pass.getOutputPath(), "_site/img");
+});
+
+test("Origin path isn’t included in output when targeting a directory several levels deep", t => {
+  let pass = new TemplatePassthrough("_src/subdir/img", "_site");
+  t.truthy(pass);
+  t.is(pass.getOutputPath(), "_site/img");
+});
+
+test("Origin path isn’t included in output when targeting a file", t => {
+  let pass = new TemplatePassthrough("_src/avatar.png", "_site");
+  t.truthy(pass);
+  t.is(pass.getOutputPath(), "_site/avatar.png");
+});
+
+test("Origin path isn’t included in output when targeting a file several levels deep", t => {
+  let pass = new TemplatePassthrough("_src/subdir/img/avatar.png", "_site");
+  t.truthy(pass);
+  t.is(pass.getOutputPath(), "_site/avatar.png");
+});
