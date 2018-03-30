7b6f2c5156e69a375afb885bae6f54f95b0542b2
Zach Leatherman <zachleatherman@gmail.com>
2018-03-30 12:59:44 -0500

    inputDir passed to TemplatePassthrough

--- test/TemplatePassthroughTest.js
+++ test/TemplatePassthroughTest.js
@@ -2,13 +2,13 @@ import test from "ava";
 import TemplatePassthrough from "../src/TemplatePassthrough";
 
 test("Constructor", t => {
-  let pass = new TemplatePassthrough("avatar.png", "_site");
+  let pass = new TemplatePassthrough("avatar.png", "_site", ".");
   t.truthy(pass);
   t.is(pass.getOutputPath(), "_site/avatar.png");
 });
 
 test("Constructor Dry Run", t => {
-  let pass = new TemplatePassthrough("avatar.png", "_site");
+  let pass = new TemplatePassthrough("avatar.png", "_site", ".");
   pass.setDryRun(true);
   t.is(pass.isDryRun, true);
 });
