b6dc9b0fee03aa185de1e7b5e0e6c8b32786860d
Zach Leatherman <zachleatherman@gmail.com>
2018-05-21 08:23:18 -0500

    Fixes #110, wraps it all in a try catch.

--- cmd.js
+++ cmd.js
@@ -8,44 +8,48 @@ const EleventyNodeVersionCheck = require("./src/VersionCheck");
 
 EleventyNodeVersionCheck().then(
   function() {
+    const chalk = require("chalk");
     const Eleventy = require("./src/Eleventy");
     const EleventyCommandCheck = require("./src/EleventyCommandCheck");
 
-    let cmdCheck = new EleventyCommandCheck(argv);
     try {
+      let cmdCheck = new EleventyCommandCheck(argv);
       cmdCheck.hasUnknownArguments();
     } catch (e) {
-      const chalk = require("chalk");
-      console.log(chalk.red(e.toString()));
+      console.log(chalk.red("Eleventy error:"), chalk.red(e.toString()));
       return;
     }
 
-    let elev = new Eleventy(argv.input, argv.output);
-    elev.setConfigPath(argv.config);
-    elev.setPathPrefix(argv.pathprefix);
-    elev.setDryRun(argv.dryrun);
-    elev.setFormats(argv.formats);
+    try {
+      let elev = new Eleventy(argv.input, argv.output);
+      elev.setConfigPath(argv.config);
+      elev.setPathPrefix(argv.pathprefix);
+      elev.setDryRun(argv.dryrun);
+      elev.setFormats(argv.formats);
 
-    let isVerbose = process.env.DEBUG ? false : !argv.quiet;
-    elev.setIsVerbose(isVerbose);
+      let isVerbose = process.env.DEBUG ? false : !argv.quiet;
+      elev.setIsVerbose(isVerbose);
 
-    elev.init().then(function() {
-      if (argv.version) {
-        console.log(elev.getVersion());
-      } else if (argv.help) {
-        console.log(elev.getHelp());
-      } else if (argv.serve) {
-        elev.watch().then(function() {
-          elev.serve(argv.port);
-        });
-      } else if (argv.watch) {
-        elev.watch();
-      } else {
-        elev.write().then(function() {
-          // do something custom if you want
-        });
-      }
-    });
+      elev.init().then(function() {
+        if (argv.version) {
+          console.log(elev.getVersion());
+        } else if (argv.help) {
+          console.log(elev.getHelp());
+        } else if (argv.serve) {
+          elev.watch().then(function() {
+            elev.serve(argv.port);
+          });
+        } else if (argv.watch) {
+          elev.watch();
+        } else {
+          elev.write().then(function() {
+            // do something custom if you want
+          });
+        }
+      });
+    } catch (e) {
+      console.log(chalk.red("Eleventy error:"), e);
+    }
   },
   function(error) {
     // EleventyNodeVersionCheck rejection
