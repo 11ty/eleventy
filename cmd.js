6d46bda1151dce8a8452d7daab28a2fa02ecfa12
Zach Leatherman <zachleatherman@gmail.com>
2018-08-14 08:22:28 -0500

    Switch from check-node-version to please-upgrade-node package for performance! Saves about 600ms on my blog build—wat

--- cmd.js
+++ cmd.js
@@ -1,57 +1,55 @@
 #!/usr/bin/env node
+const pkg = require("./package.json");
+const chalk = require("chalk"); // node 4+
+require("please-upgrade-node")(pkg, {
+  message: function(requiredVersion) {
+    return chalk.red(
+      `Eleventy requires Node ${requiredVersion}. You’ll need to upgrade to use it!`
+    );
+  }
+});
+
 if (process.env.DEBUG) {
   require("time-require");
 }
 
 const argv = require("minimist")(process.argv.slice(2));
-const EleventyNodeVersionCheck = require("./src/VersionCheck");
+const Eleventy = require("./src/Eleventy");
+const EleventyCommandCheck = require("./src/EleventyCommandCheck");
 
-EleventyNodeVersionCheck().then(
-  function() {
-    const chalk = require("chalk");
-    const Eleventy = require("./src/Eleventy");
-    const EleventyCommandCheck = require("./src/EleventyCommandCheck");
+try {
+  let cmdCheck = new EleventyCommandCheck(argv);
+  cmdCheck.hasUnknownArguments();
+} catch (e) {
+  console.log(chalk.red("Eleventy error:"), chalk.red(e.toString()));
+  return;
+}
 
-    try {
-      let cmdCheck = new EleventyCommandCheck(argv);
-      cmdCheck.hasUnknownArguments();
-    } catch (e) {
-      console.log(chalk.red("Eleventy error:"), chalk.red(e.toString()));
-      return;
-    }
+try {
+  let elev = new Eleventy(argv.input, argv.output);
+  elev.setConfigPath(argv.config);
+  elev.setPathPrefix(argv.pathprefix);
+  elev.setDryRun(argv.dryrun);
+  elev.setFormats(argv.formats);
 
-    try {
-      let elev = new Eleventy(argv.input, argv.output);
-      elev.setConfigPath(argv.config);
-      elev.setPathPrefix(argv.pathprefix);
-      elev.setDryRun(argv.dryrun);
-      elev.setFormats(argv.formats);
+  let isVerbose = process.env.DEBUG ? false : !argv.quiet;
+  elev.setIsVerbose(isVerbose);
 
-      let isVerbose = process.env.DEBUG ? false : !argv.quiet;
-      elev.setIsVerbose(isVerbose);
-
-      elev.init().then(function() {
-        if (argv.version) {
-          console.log(elev.getVersion());
-        } else if (argv.help) {
-          console.log(elev.getHelp());
-        } else if (argv.serve) {
-          elev.watch().then(function() {
-            elev.serve(argv.port);
-          });
-        } else if (argv.watch) {
-          elev.watch();
-        } else {
-          elev.write();
-        }
+  elev.init().then(function() {
+    if (argv.version) {
+      console.log(elev.getVersion());
+    } else if (argv.help) {
+      console.log(elev.getHelp());
+    } else if (argv.serve) {
+      elev.watch().then(function() {
+        elev.serve(argv.port);
       });
-    } catch (e) {
-      console.log(chalk.red("Eleventy error:"), e);
+    } else if (argv.watch) {
+      elev.watch();
+    } else {
+      elev.write();
     }
-  },
-  function(error) {
-    // EleventyNodeVersionCheck rejection
-    const chalk = require("chalk");
-    console.log(chalk.red(error.toString()));
-  }
-);
+  });
+} catch (e) {
+  console.log(chalk.red("Eleventy error:"), e);
+}
