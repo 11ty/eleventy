89129752c966bcc9b9f81bfddde831ee7324f21e
Pixelcode <Pixelcode>
2018-10-30 20:10:49 +0000

    Added two helper variables to the Pagination plugin
    Changes to Pagination:
    
    * Added firstPageLink
    * Added lastPageLink

--- test/PaginationTest.js
+++ test/PaginationTest.js
@@ -211,6 +211,51 @@ test("Permalink with pagination variables (numeric, one indexed)", async t => {
   t.is(page1Data.pagination.pageLinks.length, 2);
 });
 
+test("Permalink first and last page link with pagination variables (numeric)", async t => {
+  let tmpl = new Template(
+    "./test/stubs/paged/pagedpermalinknumeric.njk",
+    "./test/stubs/",
+    "./dist"
+  );
+
+  let data = await tmpl.getData();
+  let paging = new Pagination(data);
+  paging.setTemplate(tmpl);
+  let pages = await paging.getPageTemplates();
+
+  let page0Data = await pages[0].getData();
+  t.is(await pages[0].getOutputPath(), "./dist/paged/page-0/index.html");
+  t.is(page0Data.pagination.firstPageLink, "/paged/page-0/index.html");
+  t.is(page0Data.pagination.lastPageLink, "/paged/page-1/index.html");
+
+  let page1Data = await pages[1].getData();
+  t.is(await pages[1].getOutputPath(), "./dist/paged/page-1/index.html");
+  t.is(page1Data.pagination.firstPageLink, "/paged/page-0/index.html");
+  t.is(page1Data.pagination.lastPageLink, "/paged/page-1/index.html");
+});
+
+test("Permalink first and last page link with pagination variables (numeric, one indexed)", async t => {
+  let tmpl = new Template(
+    "./test/stubs/paged/pagedpermalinknumericoneindexed.njk",
+    "./test/stubs/",
+    "./dist"
+  );
+
+  let data = await tmpl.getData();
+  let paging = new Pagination(data);
+  paging.setTemplate(tmpl);
+  let pages = await paging.getPageTemplates();
+
+  let page0Data = await pages[0].getData();
+  t.is(await pages[0].getOutputPath(), "./dist/paged/page-1/index.html");
+  t.is(page0Data.pagination.firstPageLink, "/paged/page-1/index.html");
+  t.is(page0Data.pagination.lastPageLink, "/paged/page-2/index.html");
+
+  let page1Data = await pages[1].getData();
+  t.is(page0Data.pagination.firstPageLink, "/paged/page-1/index.html");
+  t.is(page0Data.pagination.lastPageLink, "/paged/page-2/index.html");
+});
+
 test("Alias to page data", async t => {
   let tmpl = new Template(
     "./test/stubs/paged/pagedalias.njk",
