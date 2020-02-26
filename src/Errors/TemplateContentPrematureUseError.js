fdd8adb854146e60e8f8515657f018124b3317b0
Samuel Path <samuelpath@users.noreply.github.com>
2020-02-26 17:16:41 +0100

    Make code style more coherent on error file
    In most files in the project, there is a line break between the const declarations on top and the class definition.
    
    So I'm doing the same here to add some consistency.
    
    The contrast is obvious when opening the 2 error files right after the other, as they should have the same layout but don't.

--- src/Errors/TemplateContentPrematureUseError.js
+++ src/Errors/TemplateContentPrematureUseError.js
@@ -1,4 +1,5 @@
 const EleventyBaseError = require("../EleventyBaseError");
+
 class TemplateContentPrematureUseError extends EleventyBaseError {}
 
 module.exports = TemplateContentPrematureUseError;
