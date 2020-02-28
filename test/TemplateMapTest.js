a7b89458a2e5ba03c6855701be41acabe9151009
Zach Leatherman <zachleatherman@gmail.com>
2020-02-28 08:43:26 -0600

    Make dependency graph test file

--- test/TemplateMapTest.js
+++ test/TemplateMapTest.js
@@ -749,28 +749,6 @@ test("Dependency Map should have include orphan user config collections (in the 
   t.is(collections.userCollection.length, 2);
 });
 
-test("Dependency graph assumptions", async t => {
-  const DependencyGraph = require("dependency-graph").DepGraph;
-  let graph = new DependencyGraph();
-
-  graph.addNode("all");
-  graph.addNode("template-a");
-  graph.addNode("template-b");
-  graph.addNode("template-c");
-  graph.addNode("userCollection");
-  graph.addDependency("all", "template-a");
-  graph.addDependency("all", "template-b");
-  graph.addDependency("all", "template-c");
-  graph.addDependency("userCollection", "all");
-  t.deepEqual(graph.overallOrder(), [
-    "template-a",
-    "template-b",
-    "template-c",
-    "all",
-    "userCollection"
-  ]);
-});
-
 test("Template pages should not have layouts when added to collections", async t => {
   let tm = new TemplateMap();
   let tmpl = new Template(
