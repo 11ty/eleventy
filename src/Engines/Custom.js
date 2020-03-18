c8e04f0bf767bb12d71266403822a2987477adc4
Zach Leatherman <zachleatherman@gmail.com>
2020-03-18 17:17:19 -0500

    Adds support for `init` in custom template engines (in this case to support loading Vue single file components)

--- src/Engines/Custom.js
+++ src/Engines/Custom.js
@@ -3,13 +3,18 @@ const TemplateEngine = require("./TemplateEngine");
 class CustomEngine extends TemplateEngine {
   constructor(name, includesDir) {
     super(name, includesDir);
+
+    this.entry = this.getExtensionMapEntry();
+    this.needsInit =
+      "init" in this.entry && typeof this.entry.init === "function";
+    this.initFinished = false;
   }
 
-  get compileFunction() {
+  getExtensionMapEntry() {
     if ("extensionMap" in this.config) {
       for (let entry of this.config.extensionMap) {
         if (entry.key.toLowerCase() === this.name.toLowerCase()) {
-          return entry.compile;
+          return entry;
         }
       }
     }
@@ -17,25 +22,27 @@ class CustomEngine extends TemplateEngine {
     throw Error(
       `Could not find a custom extension for ${this.name}. Did you add it to your config file?`
     );
+  }
+
+  needsToReadFileContents() {
+    if ("read" in this.entry) {
+      return this.entry.read;
+    }
+    return true;
   }
 
   async compile(str, inputPath) {
+    if (this.needsInit && !this.initFinished) {
+      await this.entry.init();
+      this.initFinished = true;
+    }
+
     // TODO generalize this (look at JavaScript.js)
-    return this.compileFunction(str, inputPath);
+    return this.entry.compile(str, inputPath);
   }
 
   get defaultTemplateFileExtension() {
-    if ("extensionMap" in this.config) {
-      for (let entry of this.config.extensionMap) {
-        if (entry.key.toLowerCase() === this.name.toLowerCase()) {
-          return entry.outputFileExtension;
-        }
-      }
-    }
-
-    throw Error(
-      `Could not find a custom extension for ${this.name}. Did you add it to your config file?`
-    );
+    return this.entry.outputFileExtension;
   }
 }
 
