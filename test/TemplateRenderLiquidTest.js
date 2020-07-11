6c8addfa4cf6d0a3f88bff325b1942e494765bd0
Zach Leatherman <zachleatherman@gmail.com>
2020-07-10 22:02:49 -0500

    Include reference to issue #680

--- test/TemplateRenderLiquidTest.js
+++ test/TemplateRenderLiquidTest.js
@@ -977,42 +977,42 @@ test("Liquid Render a false #1069", async t => {
   t.is(await fn({ falseValue: false }), "false");
 });
 
-test("Liquid Render Square Brackets dash single quotes", async t => {
+test("Liquid Render Square Brackets #680 dash single quotes", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     "<p>{{ test['hey-a'] }}</p>"
   );
   t.is(await fn({ test: { "hey-a": 1 } }), "<p>1</p>");
 });
 
-test.skip("Liquid Render Square Brackets dash single quotes spaces", async t => {
+test.skip("Liquid Render Square Brackets #680 dash single quotes spaces", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     "<p>{{ test[ 'hey-a' ] }}</p>"
   );
   t.is(await fn({ test: { "hey-a": 1 } }), "<p>1</p>");
 });
 
-test("Liquid Render Square Brackets dash double quotes", async t => {
+test("Liquid Render Square Brackets #680 dash double quotes", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     '<p>{{ test["hey-a"] }}</p>'
   );
   t.is(await fn({ test: { "hey-a": 1 } }), "<p>1</p>");
 });
 
-test.skip("Liquid Render Square Brackets dash double quotes spaces", async t => {
+test.skip("Liquid Render Square Brackets #680 dash double quotes spaces", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     '<p>{{ test[ "hey-a" ] }}</p>'
   );
   t.is(await fn({ test: { "hey-a": 1 } }), "<p>1</p>");
 });
 
-test("Liquid Render Square Brackets variable reference", async t => {
+test("Liquid Render Square Brackets #680 variable reference", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     "<p>{{ test[ref] }}</p>"
   );
   t.is(await fn({ test: { "hey-a": 1 }, ref: "hey-a" }), "<p>1</p>");
 });
 
-test.skip("Liquid Render Square Brackets variable reference array", async t => {
+test.skip("Liquid Render Square Brackets #680 variable reference array", async t => {
   let fn = await new TemplateRender("liquid").getCompiledTemplate(
     "<p>{{ test[ref[0]] }}</p>"
   );
