509802e5f62c08f1ec305f723310e236f1faea56
Zach Leatherman <zachleatherman@gmail.com>
2021-03-19 13:20:47 -0500

    Cherry pick https://github.com/11ty/eleventy/commit/cf4342acabed4de9069e314add0328ccae16feb3

--- test/BenchmarkTest.js
+++ test/BenchmarkTest.js
@@ -6,23 +6,23 @@ function between(t, value, lowerBound, upperBound) {
   t.truthy(value <= upperBound);
 }
 
-test.cb("Standard Benchmark", t => {
+test.cb("Standard Benchmark", (t) => {
   let b = new Benchmark();
   b.before();
-  setTimeout(function() {
+  setTimeout(function () {
     b.after();
-    t.truthy(b.getTotal() >= 10);
+    t.truthy(b.getTotal() >= 0);
     t.end();
-  }, 10);
+  }, 100);
 });
 
 test.cb(
   "Nested Benchmark (nested calls are ignored while a parent is measuring)",
-  t => {
+  (t) => {
     let b = new Benchmark();
     b.before();
 
-    setTimeout(function() {
+    setTimeout(function () {
       b.before();
       b.after();
       t.truthy(b.getTotal() <= 0.1);
@@ -34,17 +34,17 @@ test.cb(
   }
 );
 
-test.cb("Reset Benchmark", t => {
+test.cb("Reset Benchmark", (t) => {
   let b = new Benchmark();
   b.before();
   b.reset();
 
-  setTimeout(function() {
+  setTimeout(function () {
     b.before();
     b.after();
     t.truthy(b.getTotal() <= 0.1);
 
-    t.throws(function() {
+    t.throws(function () {
       // throws because we reset
       b.after();
     });
