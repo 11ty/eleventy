b19db35064a4b97625f34f477f06802a92e5271f
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-27 20:49:56 +0200

    docs: document AsyncEventEmitter and harmonise function signature
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/Util/AsyncEventEmitter.js
+++ src/Util/AsyncEventEmitter.js
@@ -1,13 +1,22 @@
 const EventEmitter = require("events");
 
+/**
+ * This class emits events asynchronously.
+ * It can be used for time measurements during a build.
+ */
 class AsyncEventEmitter extends EventEmitter {
+  /**
+   * @param {string} type - The event name to emit.
+   * @param {*[]} args - Additional arguments that get passed to listeners.
+   * @returns {Promise<*[]>} - Promise resolves once all listeners were invoked
+   */
   async emit(type, ...args) {
     let listeners = this.listeners(type);
-    if (!listeners.length) {
-      return;
+    if (listeners.length === 0) {
+      return [];
     }
 
-    return await Promise.all(listeners.map((h) => h.apply(this, args)));
+    return Promise.all(listeners.map((listener) => listener.apply(this, args)));
   }
 }
 
