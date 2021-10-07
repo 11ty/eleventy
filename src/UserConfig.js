24a0f2f5518b91e2a97203ae66a9f71bb6ab8cbd
Zach Leatherman <zachleatherman@gmail.com>
2021-10-06 22:09:43 -0500

    Fixes #2001.

--- src/UserConfig.js
+++ src/UserConfig.js
@@ -80,7 +80,11 @@ class UserConfig {
   }
 
   versionCheck(expected) {
-    if (!semver.satisfies(pkg.version, expected)) {
+    if (
+      !semver.satisfies(pkg.version, expected, {
+        includePrerelease: true,
+      })
+    ) {
       throw new UserConfigError(
         `This project requires the Eleventy version to match '${expected}' but found ${pkg.version}. Use \`npm update @11ty/eleventy -g\` to upgrade the eleventy global or \`npm update @11ty/eleventy --save\` to upgrade your local project version.`
       );
