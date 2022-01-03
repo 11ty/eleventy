d8603741ab720234deeddb6dc23161f59509f29a
Zach Leatherman <zachleatherman@gmail.com>
2022-01-03 14:56:57 -0600

    Tests for `defaultRenderer`

--- test/TemplateTest-CustomExtensions.js
+++ test/TemplateTest-CustomExtensions.js
@@ -181,3 +181,193 @@ test("Using eleventyDataKey to get a different key data from instance", async (t
   let data = await tmpl.getData();
   t.is(data.topLevelData, true);
 });
+
+test("Uses default renderer (no compile function) when you override an existing extension", async (t) => {
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "liquid",
+    key: "liquid",
+    compileOptions: {
+      cache: false,
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default.liquid",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is(await tmpl.render(data), "hi");
+});
+
+test("Access to default renderer when you override an existing extension", async (t) => {
+  t.plan(2);
+
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "liquid",
+    key: "liquid",
+    compileOptions: {
+      cache: false,
+    },
+    compile: function (str, inputPath) {
+      // plaintext
+      return function (data) {
+        t.true(true);
+        return this.defaultRenderer();
+      };
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default.liquid",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is(await tmpl.render(data), "hi");
+});
+
+test("Overridden liquid gets used from a markdown template", async (t) => {
+  t.plan(2);
+
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "liquid",
+    key: "liquid",
+    compileOptions: {
+      cache: false,
+    },
+    compile: function (str, inputPath) {
+      // plaintext
+      return function (data) {
+        t.true(true);
+        return this.defaultRenderer();
+      };
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default.md",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is((await tmpl.render(data)).trim(), "<p>hi</p>");
+});
+
+test("Use marked for markdown", async (t) => {
+  const { marked } = require("marked");
+
+  t.plan(2);
+
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "md",
+    key: "md",
+    compileOptions: {
+      cache: false,
+    },
+    compile: function (str, inputPath) {
+      let html = marked.parse(str);
+      // plaintext
+      return function (data) {
+        t.true(true);
+        return html;
+      };
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default-no-liquid.md",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is((await tmpl.render(data)).trim(), "<p>hi</p>");
+});
+
+test("Use defaultRenderer for markdown", async (t) => {
+  t.plan(2);
+
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "md",
+    key: "md",
+    compileOptions: {
+      cache: false,
+    },
+    compile: function (str, inputPath) {
+      return function (data) {
+        t.true(true);
+        return this.defaultRenderer(data);
+      };
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default.md",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is((await tmpl.render(data)).trim(), "<p>hi</p>");
+});
+
+test("Front matter in a custom extension", async (t) => {
+  t.plan(2);
+
+  let eleventyConfig = new TemplateConfig();
+  eleventyConfig.userConfig.extensionMap.add({
+    extension: "txt",
+    key: "txt",
+    compileOptions: {
+      cache: false,
+    },
+    compile: function (str, inputPath) {
+      return function (data) {
+        return str;
+      };
+    },
+  });
+
+  let dataObj = new TemplateData("./test/stubs/", eleventyConfig);
+  let tmpl = getNewTemplate(
+    "./test/stubs/default-frontmatter.txt",
+    "./test/stubs/",
+    "dist",
+    dataObj,
+    null,
+    eleventyConfig
+  );
+
+  let data = await tmpl.getData();
+  t.is(data.frontmatter, 1);
+  t.is((await tmpl.render(data)).trim(), "hi");
+});
