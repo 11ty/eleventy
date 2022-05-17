3140c26467841f14545b0064c8e52ad6d0f54705
Zach Leatherman <zachleatherman@gmail.com>
2022-05-17 16:46:54 -0500

    Fixes #2392. Add tests for netlify toml redirects generation. Keep redirects order in-place when an entry already exists.

---
+++ test/ServerlessPluginNetlifyRedirectsTest.js
@@ -0,0 +1,188 @@
+const test = require("ava");
+const NetlifyRedirects = require("../src/Plugins/Serverless/NetlifyRedirects");
+
+test("Starter", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects(
+    {
+      "/dynamic-path/": "./test.njk",
+    },
+    "/.netlify/builders/"
+  );
+
+  t.deepEqual(r.getResults(newRedirects), {
+    redirects: [
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+    ],
+  });
+});
+
+test("Delete redirects on empty", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects({}, "/.netlify/builders/");
+
+  t.deepEqual(r.getResults(newRedirects), {});
+});
+
+test("Add to Existing Content", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects(
+    {
+      "/dynamic-path/": "./test.njk",
+    },
+    "/.netlify/builders/"
+  );
+
+  let results = r.getResults(newRedirects, {
+    redirects: [
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+    ],
+  });
+
+  t.deepEqual(results, {
+    redirects: [
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+    ],
+  });
+});
+
+test("Add to Existing Content, exists already—keep order (serverless first)", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects(
+    {
+      "/dynamic-path/": "./test.njk",
+    },
+    "/.netlify/builders/"
+  );
+
+  let results = r.getResults(newRedirects, {
+    redirects: [
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+    ],
+  });
+
+  t.deepEqual(results, {
+    redirects: [
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+    ],
+  });
+});
+
+test("Add to Existing Content, exists already—keep order (serverless second)", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects(
+    {
+      "/dynamic-path/": "./test.njk",
+    },
+    "/.netlify/builders/"
+  );
+
+  let results = r.getResults(newRedirects, {
+    redirects: [
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+    ],
+  });
+
+  t.deepEqual(results, {
+    redirects: [
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+    ],
+  });
+});
+
+test("Remove stale path", (t) => {
+  let r = new NetlifyRedirects("possum");
+  let newRedirects = r.getNewRedirects({}, "/.netlify/builders/");
+
+  let results = r.getResults(newRedirects, {
+    redirects: [
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+      {
+        _generated_by_eleventy_serverless: "possum",
+        force: true,
+        from: "/dynamic-path/",
+        status: 200,
+        to: "/.netlify/builders/possum",
+      },
+    ],
+  });
+
+  t.deepEqual(results, {
+    redirects: [
+      {
+        force: true,
+        from: "/admin/*",
+        status: 200,
+      },
+    ],
+  });
+});
