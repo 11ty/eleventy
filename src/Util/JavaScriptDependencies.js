e5b3a4c73099f8b6f33153e609a78d3ac20770e5
Zach Leatherman <zachleatherman@gmail.com>
2023-08-18 08:53:14 -0500

    Remove unused feature

--- src/Util/JavaScriptDependencies.js
+++ src/Util/JavaScriptDependencies.js
@@ -2,27 +2,19 @@ const dependencyTree = require("@11ty/dependency-tree");
 const { TemplatePath } = require("@11ty/eleventy-utils");
 
 class JavaScriptDependencies {
-  static getDependencies(inputFiles, nodeModuleNamesOnly = false) {
+  static getDependencies(inputFiles) {
     let depSet = new Set();
 
     // TODO does this need to work with aliasing? what other JS extensions will have deps?
-    let filtered = inputFiles.filter(
-      (file) => file.endsWith(".js") || file.endsWith(".cjs")
-    );
+    let filtered = inputFiles.filter((file) => file.endsWith(".js") || file.endsWith(".cjs"));
 
     for (let file of filtered) {
       let modules = dependencyTree(file, {
-        nodeModuleNamesOnly,
+        nodeModuleNamesOnly: false,
         allowNotFound: true,
+      }).map((dependency) => {
+        return TemplatePath.addLeadingDotSlash(TemplatePath.relativePath(dependency));
       });
-
-      if (!nodeModuleNamesOnly) {
-        modules = modules.map((dependency) => {
-          return TemplatePath.addLeadingDotSlash(
-            TemplatePath.relativePath(dependency)
-          );
-        });
-      }
 
       for (let dep of modules) {
         depSet.add(dep);
--- test/JavaScriptDependenciesTest.js
+++ test/JavaScriptDependenciesTest.js
@@ -2,23 +2,8 @@ const test = require("ava");
 const JavaScriptDependencies = require("../src/Util/JavaScriptDependencies.js");
 
 test("No node_modules", (t) => {
-  t.deepEqual(
-    JavaScriptDependencies.getDependencies([
-      "./test/stubs-dependency-tree/index.js",
-    ]),
-    [
-      "./test/stubs-dependency-tree/child.js",
-      "./test/stubs-dependency-tree/grandchild.js",
-    ]
-  );
-});
-
-test("Only node_modules", (t) => {
-  t.deepEqual(
-    JavaScriptDependencies.getDependencies(
-      ["./test/stubs-dependency-tree/index.js"],
-      true
-    ),
-    ["kleur", "lodash"]
-  );
+  t.deepEqual(JavaScriptDependencies.getDependencies(["./test/stubs-dependency-tree/index.js"]), [
+    "./test/stubs-dependency-tree/child.js",
+    "./test/stubs-dependency-tree/grandchild.js",
+  ]);
 });
