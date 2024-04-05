50016f62439106a1bd3c9094c4bd41984a4c8203
Zearin <Zearin@users.noreply.github.com>
2024-04-05 16:14:57 -0400

    misc: Move guard clause before `new Set()`

--- src/TemplateCache.js
+++ src/TemplateCache.js
@@ -26,18 +26,16 @@ class TemplateCache {
 	}
 
 	add(layoutTemplate) {
-		let keys = new Set();
-
 		if (typeof layoutTemplate === "string") {
 			throw new Error(
 				"Invalid argument type passed to TemplateCache->add(). Should be a TemplateLayout.",
 			);
 		}
 
+		let keys = new Set();
 		if ("getFullKey" in layoutTemplate) {
 			keys.add(layoutTemplate.getFullKey());
 		}
-
 		if ("getKey" in layoutTemplate) {
 			// if `key` was an alias, also set to the pathed layout value too
 			// e.g. `layout: "default"` and `layout: "default.liquid"` will both map to the same template.
