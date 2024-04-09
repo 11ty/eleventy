c62dc6cedcb867c2875c2e8ae58eceb55bbfd6dd
Raphael Höser <Snapstromegon@users.noreply.github.com>
2024-04-09 16:27:30 +0200

    Transform log filter to be opionally async friendly.

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -68,9 +68,27 @@ export default function (config) {
 		return urlFilter.call(this, url, pathPrefix);
 	});
 
-	config.addFilter("log", async (input, ...messages) => {
-		console.log(await input, ...(await Promise.all(messages)));
-		return input;
+	config.addFilter("log", (input, ...messages) => {
+		const allEntries = [input, ...messages];
+		if(allEntries.some(entry => entry instanceof Promise)) {
+			// If any entry is a Promise, we switch to async bahavior.
+			// Async bahvior means that we return a Promise and await all entries before logging them to
+			// avoid logs of "Promise {pending}".
+			// After all entries are resolved and logged, the returned Promise resolves to the input value.
+
+			// additional Promise wrapper to only resolve to the original input value
+			return new Promise((res, rej) => {
+				Promise.all(allEntries).then(resolvedEntries => {
+					console.log(...resolvedEntries);
+					// We can resolve to the original input argument safely here.
+					res(input);
+				}).catch(e => rej(e))
+			})
+		} else {
+			// if all entries are sync, log them normally and return `input`
+			console.log(...allEntries);
+			return input;
+		}
 	});
 
 	config.addFilter("getCollectionItemIndex", function (collection, pageOverride) {
