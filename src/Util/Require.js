6a0b53b1310f1954a2ed8a13320550dc795b9a46
Zach Leatherman <zachleat@users.noreply.github.com>
2024-07-19 10:49:02 -0500

    Workaround for #3371

--- src/Util/Require.js
+++ src/Util/Require.js
@@ -13,7 +13,8 @@ const { port1, port2 } = new MessageChannel();
 // ESM Cache Buster is an enhancement that works in Node 18.19+
 // https://nodejs.org/docs/latest/api/module.html#moduleregisterspecifier-parenturl-options
 // Fixes https://github.com/11ty/eleventy/issues/3270
-if ("register" in module) {
+// ENV variable for https://github.com/11ty/eleventy/issues/3371
+if ("register" in module && !process?.env?.ELEVENTY_SKIP_ESM_RESOLVER) {
 	module.register("./EsmResolver.js", import.meta.url, {
 		data: {
 			port: port2,
