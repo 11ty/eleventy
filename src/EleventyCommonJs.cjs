66709b227b05fbd2bdf7fc1261e07397fd7374ba
Zach Leatherman <zachleat@users.noreply.github.com>
2024-07-24 15:47:34 -0500

    Copy tweaks for require(esm) error message

--- src/EleventyCommonJs.cjs
+++ src/EleventyCommonJs.cjs
@@ -18,7 +18,7 @@ if(!canRequireModules()) {
         const {EleventyRenderPlugin, EleventyI18nPlugin, EleventyHtmlBasePlugin} = await import("@11ty/eleventy");
       }
 
-   2. (Easier) Update the JavaScript syntax in your configuration file from CommonJS to ESM (change \`require\` to \`import\` and rename the file to have an .mjs file extension).
+   2. (Easier) Update the JavaScript syntax in your configuration file from CommonJS to ESM (change \`require\` to use \`import\` and rename the file to have an \`.mjs\` file extension).
    3. (More work) Change your project to use ESM-first by adding \`"type": "module"\` to your package.json. Any \`.js\` will need to be ported to use ESM syntax (or renamed to \`.cjs\`.)
    4. (Short term workaround) Use the --experimental-require-module flag to enable this behavior. Read more: https://nodejs.org/api/modules.html#loading-ecmascript-modules-using-require It is possible that the newest version of Node has this enabled by default—you can try upgrading your version of Node.js.`);
 
