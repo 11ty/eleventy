2ee3dfd57e4a1b2d6701b3bed8713401c21e6824
Zach Leatherman <zachleat@users.noreply.github.com>
2025-04-25 16:56:25 -0500

    Better error message.

--- src/Adapters/RetrieveGlobals.browser.js
+++ src/Adapters/RetrieveGlobals.browser.js
@@ -1,3 +1,5 @@
 export async function RetrieveGlobals(code, filePath) {
-	throw new Error("Feature not supported in browser.");
+	throw new Error(
+		"The `js` front matter type (using node-retrieve-globals for arbitrary JavaScript) is not yet supported in browser. Try using a JavaScript object {} instead!",
+	);
 }
