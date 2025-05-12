b2571a6c4de4c7dd0576b7b1c3b1f24cfb573ecf
Zach Leatherman <zachleat@users.noreply.github.com>
2025-05-12 14:36:08 -0500

    Move semver to browser-core adapter only

--- src/Adapters/Util/semver.browser-core.js
+++ src/Adapters/Util/semver.browser-core.js
@@ -1,4 +1,4 @@
-// Plugin compatibility will not be checked clientside
+// Plugin compatibility will not be checked in tiny bundle
 // Saves ~38 KB in the client bundle!
 export function satisfies() {
 	// No checking here, sorry!
