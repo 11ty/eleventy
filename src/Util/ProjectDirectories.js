e284f174c27b4e42838965b320192b505f7bfc3e
Zach Leatherman <zachleat@users.noreply.github.com>
2025-05-12 10:16:50 -0500

    Missed some tinyglobby usage outside of fs 3e50b45ca54345bd9f599aa903711f6d30828eae

--- src/Util/ProjectDirectories.js
+++ src/Util/ProjectDirectories.js
@@ -1,7 +1,7 @@
 import { existsSync, statSync } from "node:fs";
 import path from "node:path";
 import { TemplatePath } from "@11ty/eleventy-utils";
-import { isDynamicPattern } from "tinyglobby";
+import { isDynamicPattern } from "./GlobMatcher.js";
 
 /* Directories internally should always use *nix forward slashes */
 class ProjectDirectories {
