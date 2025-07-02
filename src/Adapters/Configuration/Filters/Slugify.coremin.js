7b9303bea60b35fc969a2adbae2dc9e4e776a92e
Zach Leatherman <zachleat@users.noreply.github.com>
2025-07-02 17:11:43 -0500

    Add comment about file savings

--- src/Adapters/Configuration/Filters/Slugify.coremin.js
+++ src/Adapters/Configuration/Filters/Slugify.coremin.js
@@ -1,3 +1,4 @@
+// Saves ~27KB (minified)
 export default function () {
 	throw new Error(
 		"The `slugify` filter not included with the minimal Eleventy bundle. You can add it yourself via `eleventyConfig.addFilter`",
