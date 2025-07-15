f9c6e12cd099d102a7f95ca3795c76ba687ba9de
Zach Leatherman <zachleat@users.noreply.github.com>
2025-07-15 16:26:30 -0500

    Clarity on package.json updater script, now carries engines too. Removes values from pregenerated version

--- packages/client/update-package-json.js
+++ packages/client/update-package-json.js
@@ -4,6 +4,7 @@ import corePkg from "../../package.json" with { type: "json" };
 // assign new version in local package.json from core package.json
 import clientPkg from "./package.json" with { type: "json" };
 clientPkg.version = corePkg.version;
+clientPkg.engines = corePkg.engines;
 
 fs.writeFileSync("./package.json", JSON.stringify(clientPkg, null, 2), "utf8");
 console.log(`[11ty/bundle/client] Updated @11ty/client package version to ${corePkg.version}`);
