67550050e9e6b983fa4557b42cb0dd23bbd4894a
Zach Leatherman <zachleat@users.noreply.github.com>
2025-07-17 17:15:16 -0500

    Better failure case for date parsing in minimal bundle

--- src/Adapters/luxonDatetime.coremin.js
+++ src/Adapters/luxonDatetime.coremin.js
@@ -1,3 +1,9 @@
-export function fromISOtoDateUTC(dateValue) {
-	return new Date(Date.parse(dateValue));
+export function fromISOtoDateUTC(dateValue, inputPath) {
+	let d = Date.parse(dateValue);
+	if (isNaN(d)) {
+		throw new Error(
+			`Data cascade value for \`date\` (${dateValue}) could not be parsed via Date.parse()${inputPath ? ` for ${inputPath}` : ""}. You can configure this behavior using eleventyConfig.addDateParsing: https://www.11ty.dev/docs/dates/#configuration-api-for-custom-date-parsing`,
+		);
+	}
+	return new Date(d);
 }
