59ff2a9e48b808f4bd68244cf72504a81c258a21
Zach Leatherman <zachleat@users.noreply.github.com>
2026-06-15 11:12:24 -0500

    Resolves https://github.com/11ty/eleventy/pull/4294#discussion_r3407831801

--- src/Util/EnvironmentVars.cjs
+++ src/Util/EnvironmentVars.cjs
@@ -1,7 +1,11 @@
 // Used by CommonJS upstream (cmd.cjs and TypeScript feature test)
 
 module.exports.getEnvValue = function(key) {
-	return process?.env?.[`BUILDAWESOME_${key}`] || process?.env?.[`ELEVENTY_${key}`];
+	if(!process?.env) {
+		return;
+	}
+
+	return process.env[`BUILDAWESOME_${key}`] || process.env[`ELEVENTY_${key}`];
 }
 
 module.exports.setEnvValue = function(key, value) {
